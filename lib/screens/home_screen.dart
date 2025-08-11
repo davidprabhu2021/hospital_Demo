d4ab202e25c53f656de0a0164b814ccfed978848
PRABHU R <85546905+davidprabhu2021@users.noreply.github.com>
2025-08-11 16:58:18 +0530

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,34 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'doctors_screen.dart';
 import 'appointment_screen.dart';
+import 'login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  void logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => LoginScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final userEmail = FirebaseAuth.instance.currentUser?.email ?? "User";
     return Scaffold(
-      appBar: AppBar(title: Text('Hospital Home')),
+      appBar: AppBar(
+        title: Text('Welcome, $userEmail'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () => logout(context),
+          )
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
               child: Text('View Doctors'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => DoctorsScreen()),
-                );
-              },
+              onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => DoctorsScreen())),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               child: Text('My Appointments'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => AppointmentScreen()),
-                );
-              },
+              onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => AppointmentScreen())),
             ),
           ],
         ),
